3043c3e1fbd0318723b49f1df84d6fe0eec61763
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-09-14 18:05:07 -0300

    Removidos contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+
+from recipes.views import home
 
 urlpatterns = [
-    path('', home),  # home
-    path('sobre/', sobre),  # /sobre/
-    path('contato/', contato),  # /contato/
+    path('', home),
 ]
