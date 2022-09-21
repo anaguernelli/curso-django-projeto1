45c180b0d3f528c6ba0f48f8d0b1fef2efcdd4d9
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-09-21 20:04:49 -0300

    STATIC_ROOT

--- projeto/settings.py
+++ projeto/settings.py
@@ -119,8 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
