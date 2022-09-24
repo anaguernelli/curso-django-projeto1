dbe05e072b9beb6f6455654b2e0ec97f7adbc4f0
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-09-24 19:10:25 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render 
 # lê e renderiza o arquivo (deixa aparecer no código fonte o HTML, etc)
+from utils.recipes.factory import make_recipe
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Aninha',
-    }) 
-    
-# adicionar a pasta em q está o arquivo p não haver colisões
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Aninhaaaa',
-    }) 
-
+        'recipe': make_recipe(),
+    })
