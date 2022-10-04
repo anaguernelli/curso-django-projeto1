52bb941d5d089a8c9128b51a430cfd7b49f70e64
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-10-04 16:17:38 -0300

    Iniciando criação do primeiro model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,37 @@
+# from email.policy import default
+# from unittest.util import _MAX_LENGTH
+
 from django.db import models
 
-# Create your models here.
+# Models converte o código em tabelas de db 
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65) 
+    # CharField atua como varChar do bd
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    # para adicionar a data de 'lançamento', e não está sujeito a mudança
+    updated_at = models.DateTimeField(auto_now=True)
+    # para atualizar sempre que houver alterações
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+
+
+# EDITED
+# title description slug
+# preparation_time preparation_time_unit
+# servings servings_unit
+# preparation_step
+# preparation_step_is_html
+# created_at updated_at
+# is_published
+# cover
+# category (Relação)
+# Author (Relação)
