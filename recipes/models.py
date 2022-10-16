81051d1364e7844c004e6d8c7f79a4d3966e5d67
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-10-15 22:11:03 -0300

    Corrigindo category com blank True e default None

--- recipes/models.py
+++ recipes/models.py
@@ -29,7 +29,8 @@ class Recipe(models.Model):
     cover = models.ImageField(
       upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, 
+        default=None,
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
