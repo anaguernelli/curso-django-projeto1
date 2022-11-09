7eec2679b91d49de0944410f3c7b8ba947c4dab4
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-11-09 15:13:56 -0300

    Iniciando testes com Pytest/Pytest-Django/Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variavel = '123456'
+        print('OLÁ, MUNDO!')
+        assert 1 == 2, 'Um é igual a um'
