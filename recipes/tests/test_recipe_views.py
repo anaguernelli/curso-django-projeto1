3504a67a238b62c618e9dd276d9083fffb251398
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-11-14 20:07:38 -0300

    Separando os testes em um package chamado tests com __init__.py

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -5,20 +5,6 @@ from recipes import views
 
 # O 'resolve' resolve qual função está sendo usada
 
-
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-        
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
