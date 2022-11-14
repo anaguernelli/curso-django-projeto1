e25de42c7a8adacf98e7dca6d64e530b47fab8c2
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-11-14 10:50:17 -0300

    usando func resolve

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from recipes import views
+
+# O 'resolve' resolve qual função está sendo usada
 
 
 class RecipeURLsTest(TestCase):
@@ -15,3 +19,18 @@ class RecipeURLsTest(TestCase):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
 
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        # pôr só '/' na url é hard coded, desta forma acima 
+        # está trabalhando de forma dinâmica
+        self.assertIs(view.func, views.home)
+        # está comparando se a função view É a view de home
+
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
