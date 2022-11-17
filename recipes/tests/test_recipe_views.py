981bec3bc0cbb2681fc00c5c809335f45c4ebd75
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-11-17 10:43:11 -0300

    Primeira fixture para teste único

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 # O 'resolve' resolve qual função está sendo usada
 
@@ -29,6 +29,32 @@ class RecipeViewsTest(TestCase):
 # o content é o conteúdo HTML de recipes:home e está sendo imposto como string
 # enquanto q o assertIn() procura pro 'recipes not found' dentro deste content
 
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True
+        )
+        assert 1 == 1
+
+        
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id': 1000})
