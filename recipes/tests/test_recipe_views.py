ca1191958a0eb05c9e4502cdc78f7c09f30379df
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-11-18 08:36:38 -0300

    Criando uma classe de base para os testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,18 +1,21 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
+
 from recipes import views
-from recipes.models import Category, Recipe, User
 
-# O 'resolve' resolve qual função está sendo usada
+from .test_recipe_base import RecipeTestBase
 
-class RecipeViewsTest(TestCase):
+
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()
+
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
-        # pôr só '/' na url é hard coded, desta forma acima 
+        # pôr só '/' na url é hard coded, desta forma acima
         # está trabalhando de forma dinâmica
         self.assertIs(view.func, views.home)
         # está comparando se a função view É a view de home
-    
+
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
@@ -20,7 +23,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
-    
+
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
@@ -30,28 +33,6 @@ class RecipeViewsTest(TestCase):
 # enquanto q o assertIn() procura pro 'recipes not found' dentro deste content
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True
-        )
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
@@ -62,13 +43,13 @@ class RecipeViewsTest(TestCase):
         self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
         ...
-        
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertIs(view.func, views.category)
-    
+
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
             reverse('recipes:category', kwargs={'category_id': 1000})
@@ -80,7 +61,7 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
-    
+
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000})
