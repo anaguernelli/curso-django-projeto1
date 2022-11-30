dbaabb80f0d17bf6c1b4bda9dc9dc7b4c298bfe2
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-11-30 18:45:31 -0300

    parameterized import parameterized

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,6 @@
 from .test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -7,9 +8,14 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        self.recipe.title = 'a' * 66
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time', 65),
+        ('servings_unit', 65),
+    ])
 
-        # ele vai retornar OK após executar o full_clean e retornar um erro
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'a' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
