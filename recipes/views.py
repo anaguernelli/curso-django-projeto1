702056fcf38883aa7d47d11e768f113e0d1e70b2
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-12-14 10:13:15 -0300

    Filtro de campo contains e consultas complexas com Q

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,11 @@
+from django.http import Http404
+
+from django.db.models import Q
+
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+
 # lê e renderiza o arquivo (deixa aparecer no código fonte o HTML, etc)
 from .models import Recipe
-from django.http import Http404
-# Create your views here.
 
 
 def home(request):
@@ -41,6 +44,13 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
+        'search_term': search_term,
+        'recipes': recipes,
     })
