3b221bb163e5b8c3a79092d8c14ccfd8a64aff08
Ana Guernelli <anabeatrizmgp@gmail.com>
2022-12-27 10:47:43 -0300

    Modificando o range de páginas apenas quando necessário

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,6 +1,20 @@
+from math import ceil
+
+
 def make_pagination_range(
     page_range,
     qty_pages,
     current_page,
 ):
-    return [1, 2, 3, 4]
+    middle_range = ceil(qty_pages / 2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset
+
+    # fatiamento do range de  0 até 4
+    return page_range[start_range:stop_range]
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -12,3 +12,37 @@ class paginationTest(TestCase):
         )
 
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+        # Current page = 1 - Qty pages = 2 - Middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1
+        )
+        self.assertEqual([1, 2, 3, 4],  pagination)
+
+        # Current page = 2 - Qty pages = 2 - Middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Current page = 3 - Qty pages = 2 - Middle page = 2
+        # RANGE SHOULD CHANGE HERE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        # Current page = 4 - Qty pages = 2 - Middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )
+        self.assertEqual([3, 4, 5, 6], pagination)
