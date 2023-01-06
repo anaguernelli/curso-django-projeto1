2dce12f8fbf249cad9f290f15601b43b16b32c11
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-01-05 21:52:13 -0300

    campos manualmente

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,10 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
