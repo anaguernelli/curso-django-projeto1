1087a4dc692d3430c6a4552875cd06969f9fcf4e
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-01-05 21:50:49 -0300

    Campos manualmente

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm
+
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
