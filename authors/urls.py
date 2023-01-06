bdb44a9f691e0b8009ba3c5b0bbbc3ff3288ab80
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-01-06 14:38:57 -0300

    Enviando POST p outra URL e usando session p salvar valores entre requests

--- authors/urls.py
+++ authors/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'authors'
+
 urlpatterns = [
-    path('register/', views.register_view, name='register',)
+    path('register/', views.register_view, name='register',),
+    path('register/create/', views.register_create, name='create',),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from django.http import Http404
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    # exibe o formulário
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+    # form que já tem dados, se tiver erros vai exibir, 
+    # se tiver validado vai validar etc
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    # trata os dados do formulário
+    if not request.POST:
+        raise Http404()
+        # se o método que vir no create não for POST, for GET
+        # vai levantar um erro
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    # salvando o dicionário do POST inteiro
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
+    # redirecionar para a view register_view
