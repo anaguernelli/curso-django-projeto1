2a83282f3abc95c18619cb2eb0e8ec9156f1d8c5
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-01-09 21:36:02 -0300

    integration test

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
+from unittest import TestCase
+from django.test import TestCase as DjangoTestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
+from django.urls import reverse
 
 
+# testes unitários
 class AuthorRegisterFormUnitTest(TestCase):
     @parameterized.expand([
         ('username', 'Your username'),
@@ -47,3 +50,27 @@ class AuthorRegisterFormUnitTest(TestCase):
         current = form[field].field.label
 
         self.assertEqual(current, needed)
+
+
+# testes d e integração
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@anymail.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword1',
+    }
+
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
