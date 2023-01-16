6d9981c17d9c5806724e5e28d9843b5bb4b24b2f
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-01-16 17:46:12 -0300

    Pytest markers

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -2,7 +2,14 @@ from selenium.webdriver.common.by import By
 
 from .base import RecipeBaseFunctionalTest
 
+import pytest
 
+
+@pytest.mark.functional_test
+# para executar o marker
+# pytest -m 'functional_test' -rP
+# para executar tudo menos 'functional_test'
+# pytest -m 'not functional_test'
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipe_not_found_messages(self):
         # live_server_url vai apenas pegar nossa url sem precisarmo digitá-la
