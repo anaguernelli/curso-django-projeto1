b71b11ed80436745e65919c3cca002c56dea3d15
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-02 19:22:42 -0300

    Criando um model de perfis de usuários (Profile)

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# estamos, basicamente, criando o User de outra forma
+User = get_user_model()
+
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
