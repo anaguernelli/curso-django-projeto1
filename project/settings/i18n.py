b670f1324674a001a512250b18cfac9b24301654
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-03 11:23:45 -0300

    Configurando USE_I18N, LOCALE_PATH e middleware

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,10 +1,19 @@
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
+from . import BASE_DIR
 
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
+# caso não usar o site para outros idiomas
+# pode colocar abaixo como False, melhora
+# minimamente a rapidez do site, caso contrário, True
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    # para passar o caminho das traduções
+    BASE_DIR / 'locale'
+]
