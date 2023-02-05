cfa07ef4a497626782d04e2378d8ec5ca4c0026b
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-05 10:06:05 -0300

    redimensionando imgs no save do Model com o Pillow

--- recipes/models.py
+++ recipes/models.py
@@ -8,6 +8,9 @@ from django.db.models.functions import Concat
 from tag.models import Tag
 from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
+import os
+from django.conf import settings
+from PIL import Image
 
 
 class Category(models.Model):
@@ -67,15 +70,51 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("recipes:recipe", args={self.id, })
 
+    @staticmethod
+    def resize_image(image, new_width=800):
+        # uma junção entre o media_root e nome da img
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        # pegando a imagem do pillow
+        image_pillow = Image.open(image_full_path)
+        # size "desempacota" do 1 e 2 o atributo da tupla da variável
+        original_width, original_height = image_pillow.size
+
+        # se for menor q o max de largura, não executará nada
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        # se for maior que 800px, reduziremos essa img
+        new_height = round((new_width * original_height) / original_width)
+
+        # LANCZOS utilizado pra fazer resize da img
+        # (diminui oa img mantendo as proporções)
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=60,
+        )
+
     # Criar slug
     def save(self, *args, **kwargs):
-        # Toda vez q o User escrever o título, vai gerar uma slug com o título
+        # pre-save
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except FileNotFoundError:
+                ...
+
         # nao esqueça de retornar oq está sobrescrevendo
-        return super().save(*args, **kwargs)
+        return saved
+        # post-save
 
     def clean(self, *args, **kwargs):
         # dicionário em que agr toda chave é uma lista
@@ -94,7 +133,6 @@ class Recipe(models.Model):
 
         if error_messages:
             raise ValidationError(error_messages)
-
 
     class Meta:
         verbose_name = _('Recipe')
