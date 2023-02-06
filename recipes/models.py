58b6f9078e95f583114314a9fcd0c6cddc934592
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-06 11:40:21 -0300

    Campos relacionados (PrimaryKeyRelatedField, StringRelatedField) no Django Rest Framework

--- recipes/models.py
+++ recipes/models.py
@@ -30,7 +30,9 @@ class RecipeManager(models.Manager):
                 F('author__last_name'), Value(' ('),
                 F('author__username'), Value(')'),
             )
-        ).order_by('-id')
+        ).order_by('-id') \
+            .select_related('category', 'author') \
+            .prefetch_related('tags')
 
 
 class Recipe(models.Model):
--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,13 @@
 # converte um model para um formato que seja entendível como JSON
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -10,6 +18,15 @@ class RecipeSerializer(serializers.Serializer):
     preparation = serializers.SerializerMethodField(
         method_name='any_method_name'
     )
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True
+    )
+    tag_objects = TagSerializer(many=True, source='tags')
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
