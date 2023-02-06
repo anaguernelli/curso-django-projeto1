1df866879b4a3416e67cd6b51c645fcd9352f3ae
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-06 09:33:44 -0300

    Utilizando um serializer

--- recipes/urls.py
+++ recipes/urls.py
@@ -50,7 +50,12 @@ urlpatterns = [
     path(
         'recipes/api/v2/',
         views.recipe_api_list,
-        name='recipe_api_v2'
+        name='recipes_api_v2',
+    ),
+    path(
+        'recipes/api/v2/<int:pk>/',
+        views.recipe_api_detail,
+        name='recipes_api_v2_detail',
     ),
 ]
 
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,9 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from ..models import Recipe
+from ..serializers import RecipeSerializer
+# from rest_framework.status import status
 
 
 @api_view()
 def recipe_api_list(request):
-    # request são dados do cliente e temos
-    # que retornar uma resposta
-    return Response('ok')
+    recipes = Recipe.objects.get_published()[:10]
+    serializer = RecipeSerializer(instance=recipes, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def recipe_api_detail(request, pk):
+    recipe = get_object_or_404(
+        Recipe.objects.get_published(),
+        pk=pk
+    )
+    serializer = RecipeSerializer(instance=recipe, many=False)
+    return Response(serializer.data)
+
+    # o serializer está esperando um objeto, então temos
+    # apenas declarar q a lista/queryset tem muitos elementos
