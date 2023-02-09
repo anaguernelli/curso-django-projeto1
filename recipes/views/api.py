b5cb845df60a24d9d5c095034b9fe28503795029
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-09 11:37:57 -0300

    Sobrescrevendo get_queryset

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -12,14 +12,33 @@ class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 1
 
 
-# teremos que especificar no as_views() na urls.py
-# No ModelView, não é prudente sobrescrever um método HTTP
 class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = PageNumberPagination
 
-    # ao invés de patch, use parcial_update, se utilizar delete, use detroy...
+    # def get_serializer_class(self):
+    #     return super().get_serializer_class()
+
+    # def get_serializer(self, *args, **kwargs):
+    #     return super().get_serializer(*args, **kwargs)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["example"] = 'This is in context'
+    #     return context
+
+    def get_queryset(self):
+        # &category_id=1
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
+
     def parcial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         recipe = self.get_queryset().filter(pk=pk).first()
