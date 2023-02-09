2911b8f1d89a022d08c9f32e34b6aaa8910d768a
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-02-08 21:27:13 -0300

    Mixins, Generic Views e Pagination no Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -6,30 +6,40 @@ from tag.models import Tag
 from ..serializers import RecipeSerializer, TagSerializer
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={'request': request}
-        )
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 1
 
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request}
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = PageNumberPagination
+
+    # def get(self, request):
+    #     recipes = Recipe.objects.get_published()[:10]
+    #     serializer = RecipeSerializer(
+    #         instance=recipes,
+    #         many=True,
+    #         context={'request': request}
+    #     )
+
+    #     return Response(serializer.data)
+
+    # def post(self, request):
+    #     serializer = RecipeSerializer(
+    #         data=request.data,
+    #         context={'request': request}
+    #     )
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(
+    #         serializer.data,
+    #         status=status.HTTP_201_CREATED
+    #     )
 
 
 class RecipeAPIv2Detail(APIView):
